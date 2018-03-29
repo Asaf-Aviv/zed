4ae241aab5fbf804e120ff2fe1a87d6c3a32b610
AsafAviv <asafaviv89@gmail.com>
2018-03-29 10:19:41 +0300

    ui changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', {
         title: 'Home | Legends'
     });

06853a858009f47b60b48a676f669d6770127ae5
AsafAviv <asafaviv89@gmail.com>
2018-05-29 14:17:50 +0300

    minor bug

--- util/redis_client.js
+++ util/redis_client.js
@@ -3,6 +3,5 @@ const bluebird    = require('bluebird');
 const redisClient = require('redis').createClient();
 
 bluebird.promisifyAll(redisClient);
-bluebird.promisifyAll(redisClient);
 
 module.exports = redisClient;
